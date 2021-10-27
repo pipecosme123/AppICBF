f4c4273c58ae812515cabdae610bf0297d104b33
Daniel Cosme <pipecosme123@gmail.com>
2021-10-27 17:14:13 -0500

    Recibiendo datos el front y almacenando en la BD

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const routes = require(__dirname + '/routers/getData.js');
 const routesInsert = require(__dirname + '/routers/insertData.js');
 
 const app = express();
 
 // app.set('port', )
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.urlencoded({
    extended: false
--- api/routers/insertData.js
+++ api/routers/insertData.js
@@ -3,7 +3,6 @@ const morgan = require('morgan');
 const mysqlConnection = require('./database.js');
 const app = express();
 
-
 app.use(morgan('dev'));
 app.use(express.urlencoded({
     extended: false
@@ -11,15 +10,143 @@ app.use(express.urlencoded({
 app.use(express.json());
 
 
-app.get('/prueba', (req, res) => {
-    mysqlConnection.query('SELECT * FROM Pacientes;', (err, row, fields) => {
+app.post('/infoPaciente', (req, res) => {
+
+    const {
+        noDocumentoPaciente,
+        nombrePaciente,
+        apellidoPaciente,
+        fechaNacimiento,
+        lugarNacimiento,
+        ubicacionLugarNacimiento,
+        direccionResidencial,
+        barrioResidencial,
+        ciudadResidencial,
+        ubicacionResidencial,
+        escuela,
+        ubicacionEscuela,
+        eps,
+        acudienteIdentificación,
+        acudienteNombre,
+        acudienteApellido,
+        acudienteTeléfono,
+        acudienteParentesco
+    } = req.body;
+
+    const query = "call insertData_Paciente(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"
+
+    mysqlConnection.query(query, [
+        noDocumentoPaciente,
+        nombrePaciente,
+        apellidoPaciente,
+
+        fechaNacimiento,
+        lugarNacimiento,
+        ubicacionLugarNacimiento,
+
+        direccionResidencial,
+        barrioResidencial,
+        ciudadResidencial,
+        ubicacionResidencial,
+
+        escuela,
+        ubicacionEscuela,
+
+        eps,
+
+        acudienteIdentificación,
+        acudienteNombre,
+        acudienteApellido,
+        acudienteTeléfono,
+        acudienteParentesco
+    ],
+        (err, row, fields) => {
+            if (!err) {
+                let response = row[0];
+
+                response.map((resp, index) => {
+                    res.json(resp.Respuesta);
+                })
+                // res.send(response);
+            } else {
+                console.log(err);
+            }
+        })
+});
+
+app.post('/antecedentes', (req, res) => {
+
+    const {
+        documentoPaciente,
+        ultimaConsulta,
+        motivoConsulta,
+        motivoConsulta_cual,
+        fechaControlMedico,
+        talla,
+        peso,
+        ComplicacionEmbarazo,
+        ComplicacionEmbarazo_cual,
+        tiempoEsperado,
+        enfermedadAntiguna,
+        enfermedadAntiguna_cual,
+        hospitalizacion,
+        hospitalizacion_cuantoTiempo,
+        hospitalizacion_porque,
+        medicamento,
+        medicamento_cual,
+        cirugia,
+        cirugia_cual,
+        cirugia_haceCuanto,
+        cuidadoEspecial,
+        cuidadoEspecial_cual,
+        textPregunta72,
+        ultimaConsultaOdontologo,
+        ultimaConsultaOdontologo_CadaCuanto,
+        motivoUltimaConsulta,
+        motivoUltimaConsulta_haceCuanto,
+        experiencias
+    } = req.body;
+
+    const query = "INSERT INTO Antecedentes VALUES(NULL,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);"
+
+    mysqlConnection.query(query, [
+        documentoPaciente,
+        ultimaConsulta,
+        motivoConsulta,
+        motivoConsulta_cual,
+        fechaControlMedico,
+        talla,
+        peso,
+        ComplicacionEmbarazo,
+        ComplicacionEmbarazo_cual,
+        tiempoEsperado,
+        enfermedadAntiguna,
+        enfermedadAntiguna_cual,
+        hospitalizacion,
+        hospitalizacion_cuantoTiempo,
+        hospitalizacion_porque,
+        medicamento,
+        medicamento_cual,
+        cirugia,
+        cirugia_cual,
+        cirugia_haceCuanto,
+        cuidadoEspecial,
+        cuidadoEspecial_cual,
+        textPregunta72,
+        ultimaConsultaOdontologo,
+        ultimaConsultaOdontologo_CadaCuanto,
+        motivoUltimaConsulta,
+        motivoUltimaConsulta_haceCuanto,
+        experiencias
+    ], 
+    (err, row, fields) => {
         if (!err) {
             res.json(row);
-            // console.log(res)
         } else {
             console.log(err);
         }
-    })
+    }
+    );
 });
 
 module.exports = app;
